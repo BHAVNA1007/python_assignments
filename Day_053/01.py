1ae5f21daa9940e02ec3fa5f769701f519f9e5c5
BHAVNA1007 <bhavnapotphode7@gmail.com>
2026-09-21 22:01:42 +0530

    Matrix Transpose & Diagonal Transformation

--- Day_053/01.py
+++ Day_053/01.py
@@ -54,3 +54,69 @@ Do not use zip().
 Do not use built-in matrix operations.
 Do not modify the original matrix.
 '''
+
+
+n = int(input("Enter the size of matrix: "))
+matrix = []
+for i in range(n):
+    rows  = []
+    for j in range(n):
+        rows.append(int(input()))
+    matrix.append(rows)
+
+# print(matrix)      
+
+print("Original Matrix:")
+for i in matrix:
+    for j in i:
+        print(j, end =" ") 
+    print()
+
+print("Main Diagonal Elements:")
+for i in range(n):
+    print(matrix[i][i], end = " ")
+print()
+
+print("Secondary Diagonal Elements:")
+
+
+for i in range(n):
+    for j in range(n):
+        if i+j == n-1:
+            print(matrix[i][j], end = " ")
+print()            
+
+transpose = []
+for i in range(n):
+    row = []
+    for j in range(n):
+        row.append(matrix[j][i])
+    transpose.append(row)        
+
+print("Transpose Matrix:")
+for i in transpose:
+    for j in i:
+        print(j, end = " ")
+    print()        
+
+md = []
+sd = []
+for i in range(n):
+    for j in range(n):
+        if i==j:
+            md.append(transpose[i][j])
+        if i+j == n-1:
+            sd.append(transpose[i][j])
+
+for i in range(n):
+    for j in range(n):
+        if i==j:
+            transpose[i][j] = sd[i]    
+        if i+j == n-1:
+            transpose[i][j] = md[i]    
+
+print("Final Matrix After Diagonal Swapping:")
+for i in transpose:
+    for j in i:
+        print(j, end = " ")
+    print()        
